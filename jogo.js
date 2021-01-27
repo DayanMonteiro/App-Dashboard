7a792999f8e846840352a5ffa7a2a4220e40da81
Dayan Monteiro <dmsmamb@gmail.com>
2021-01-27 11:44:16 -0300

    aparição de tamanhos variados de mosquitos

--- jogo.js
+++ jogo.js
@@ -2,30 +2,49 @@
 var altura = 0
 var largura = 0
 
-function ajustaTamanhoPalcoJogo() { 
-    var altura = window.innerHeight
-    var largura = window.innerWidth
+function ajustaTamanhoPalcoJogo() {
+	altura = window.innerHeight
+	largura = window.innerWidth
 
-    console.log(altura, largura)
+	console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
-function posicaoRandomica() { 
 
-var posicaoX = Math.floor(Math.random() * largura)
-var posicaoY = Math.floor(Math.random() * altura)
+function posicaoRandomica() {
 
-console.log(posicaoX, posicaoY)
+	var posicaoX = Math.floor(Math.random() * largura) - 90
+	var posicaoY = Math.floor(Math.random() * altura) - 90
 
-// criar o elemento html
-var mosquito =  document.createElement('img')
-mosquito.src = './img/mosquito.png'
-mosquito.className = 'mosquito1'
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+	posicaoX = posicaoX < 0 ? 0 : posicaoX
+	posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-document.body.appendChild(mosquito)
+	console.log(posicaoX, posicaoY)
 
+	//criar o elemento html
+	var mosquito = document.createElement('img')
+	mosquito.src = 'img/mosquito.png'
+	mosquito.className = tamanhoAleatorio()
+	mosquito.style.left = posicaoX + 'px'
+	mosquito.style.top = posicaoY + 'px'
+	mosquito.style.position = 'absolute'
+
+	document.body.appendChild(mosquito)
+
+}
+
+
+function tamanhoAleatorio() {
+	var classe = Math.floor(Math.random() * 3)
+	console.log(classe)
+
+	switch(classe) {
+		case 0:
+			return 'mosquito1'
+		case 1:
+			return 'mosquito2'
+		case 2:
+			return 'mosquito3'
+	}
 }
