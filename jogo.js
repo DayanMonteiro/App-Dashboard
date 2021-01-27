b37c99983e15f5a6d3bc6c9c4a0b605256821504
Dayan Monteiro <dmsmamb@gmail.com>
2021-01-27 11:58:32 -0300

    fazendo o mosquito virar pra esquerda e direita

--- jogo.js
+++ jogo.js
@@ -25,19 +25,20 @@ function posicaoRandomica() {
 	//criar o elemento html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'img/mosquito.png'
-	mosquito.className = tamanhoAleatorio()
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
 
 	document.body.appendChild(mosquito)
 
+	console.log()
+
 }
 
 
 function tamanhoAleatorio() {
 	var classe = Math.floor(Math.random() * 3)
-	console.log(classe)
 
 	switch(classe) {
 		case 0:
@@ -46,5 +47,16 @@ function tamanhoAleatorio() {
 			return 'mosquito2'
 		case 2:
 			return 'mosquito3'
+	}
+}
+
+function ladoAleatorio() {
+	var classe = Math.floor(Math.random() * 2)
+	
+	switch(classe) {
+		case 0:
+			return 'ladoA'
+		case 1:
+			return 'ladoB'
 	}
 }
