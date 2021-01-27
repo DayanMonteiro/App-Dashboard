39f1e63f99b784dd51731efeb5dd8bb344f57596
Dayan Monteiro <dmsmamb@gmail.com>
2021-01-27 14:26:50 -0300

    aparição e verificação de moscas

--- jogo.js
+++ jogo.js
@@ -14,6 +14,12 @@ ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
+	// verificando se o mosquito existe
+	if(document.getElementById('mosquito')) {
+	// remover o mosquito anterior (caso exista)
+	document.getElementById('mosquito').remove()
+	}
+
 	var posicaoX = Math.floor(Math.random() * largura) - 90
 	var posicaoY = Math.floor(Math.random() * altura) - 90
 
@@ -29,6 +35,8 @@ function posicaoRandomica() {
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.style.position = 'absolute'
+
+	mosquito.id = 'mosquito'
 
 	document.body.appendChild(mosquito)
 
